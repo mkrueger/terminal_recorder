41033a738ee71328f7058aa02ba4155b4bb24b1d
Mike Krüger <mikkrg@microsoft.com>
2024-02-14 12:20:35 +0100

    Added test script.

---
+++ test.sh
@@ -0,0 +1,5 @@
+export APPLICATIONINSIGHTS_CONNECTION_STRING=InstrumentationKey=d8d95f39-1074-4e19-a6f2-4d16f997a757;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/
+export MAVEN_OPTS=-javaagent:/home/azure_user/terminal_recorder/applicationinsights-agent-3.4.19.jar
+
+#mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="-u test_user create"
+mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="-u test_user -f data\test.cast test"
--- upload.bat
+++
@@ -1,3 +0,0 @@
-mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="-u test_user create"
-mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="-u test_user -f data\test.cast upload"
-mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="-u test_user -f millenium delete"
