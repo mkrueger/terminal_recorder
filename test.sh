03df7262e11dd0917f8511454622638e3cd2b234
Mike Krüger <mikkrg@microsoft.com>
2024-02-14 12:45:40 +0100

    Fixed test script.

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ export APPLICATIONINSIGHTS_CONNECTION_STRING=InstrumentationKey=d8d95f39-1074-4e
 export MAVEN_OPTS=-javaagent:/home/azure_user/terminal_recorder/applicationinsights-agent-3.4.19.jar
 
 #mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="-u test_user create"
-mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="-u test_user -f data\test.cast test"
+mvn compile exec:java -Dexec.mainClass="org.example.Main" -Djavaagent:applicationinsights-agent-3.4.19.jar -Dexec.args="-u test_user -f ./data/test.cast test"
